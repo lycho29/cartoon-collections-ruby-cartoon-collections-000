87a3270ba7caa17765551d5a8ac176bf03ff3465
Lydia Cho <lydiacho@gse.havard.edu>
2020-10-02 16:43:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-#this is correct
+
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |item, index| 
-    index += 1
-    puts "#{index}: #{item}"
+  half_dwarves = dwarves.length / 2
+
+    dwarves.each_with_index do |name, index| 
+      if index += 1 <= half_dwarves 
+      puts "#{index}: #{name}"
   end
 end
 
-#this is correct
+
 planteer_calls = ["earth", "wind", "fire", "water", "heart"]
 veggies = ["carrot", "cucumber", "pepper"]
 fruits = ["apple", "banana", "orange"]
@@ -17,25 +19,32 @@ def summon_captain_planet(veggies)
 end
 
 
-#this is correct
-long_planeteer_calls = ["earth", "wind", "fire", "water", "heart"] 
 
-def long_planeteer_calls(long_planeteer_calls)
-  long_planeteer_calls.any? do |word|
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"] 
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |word|
     word.length > 4
   end
 end
 
 
 def find_the_cheese(food)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  food.find do |cheese|
-    cheese_types.include?(cheese)
-  end
-end
+  cheese_types = ["cheddar", "gouda", "camembert", "swiss"]
+  food.find {|cheese| cheese_types.include?(cheese)}
+end 
 
-#this is correct
 #def find_the_cheese(cheese_types)
  # no_cheese = ["ham", "cellphone", "computer"]
 #  cheese_types.index("cheddar")
 #end
+
+def word_with_b(words)
+  array = []
+  words.each do |word|
+    if word[0] == "b"
+      array << word
+    end
+  end
+    array
+end
